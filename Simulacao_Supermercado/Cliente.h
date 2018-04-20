2454d0f3f6aab72f063d936b958f371e124cfdee
RenanDeFreitas <37121384+RenanDeFreitas@users.noreply.github.com>
2018-04-20 12:40:23 -0300

    Update Cliente.h

--- Simulacao_Supermercado/Cliente.h
+++ Simulacao_Supermercado/Cliente.h
@@ -21,10 +21,10 @@ void chegadaDeClientes(Fila<T> *caixa, int qtdFilas) {
 
 	for (int i = 0; i < 10; i++)
 	{
-		if (i == 2) { //Os clientes 2, 3 e 4 v„o demorar 2 unidades de tempo para serem atendidos
+		if (i == 2) { //Os clientes 2, 3 e 4 v√£o demorar 2 unidades de tempo para serem atendidos
 			unidadeTempo++;
 		}
-		if (i == 5) { //Os clientes 5, 6, 7, 8 e 9 v„o demorar 3 unidades de tempo para serem atendidos
+		if (i == 5) { //Os clientes 5, 6, 7, 8 e 9 v√£o demorar 3 unidades de tempo para serem atendidos
 			unidadeTempo++;
 		}
 		Cliente *cliente = new Cliente(unidadeTempo); //Cria o cliente e adiciona uma unidade de tempo u.t
@@ -39,7 +39,7 @@ void chegadaDeClientes(Fila<T> *caixa, int qtdFilas) {
 			}
 		}
 
-		enfileirar(caixa[numeroFilaMenor], *cliente); //Passa o caixa com menor fila e o conteudo do ponteiro cliente para ser enfileirado
+		enfileirar(caixa[numeroFilaMenor], cliente); //Passa o caixa com menor fila e o conteudo do ponteiro cliente para ser enfileirado
 
 	}
 
@@ -52,7 +52,7 @@ void atendimentoDeClientes(Fila<T> *caixa, int qtdFilas) {
 			caixa[i].inicio.tempo--; //diminui 1 u.t (unidade de tempo)
 		}
 		else {
-			desenfileirar(caixa[i]); //Desenfileirar n„o precisa de T dado em fila.h, arrumar isso
+			desenfileirar(caixa[i]); //Desenfileirar n√£o precisa de T dado em fila.h, arrumar isso
 		}
 	}
 }
